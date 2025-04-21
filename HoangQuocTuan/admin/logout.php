4dd386add862a4b1bbc8376b100189c986220805
HQTUAN1410 <hoangquoctuan1014@gmail.com>
2025-04-21 22:30:35 +0700

    Delete HoangQuocTuan directory

--- HoangQuocTuan/admin/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-
-if(isset($_SESSION['USER']) && $_SESSION['USER'] != NULL){
-    unset($_SESSION['USER']);
-    header('Location: login.php');
-}
-?>
--- HoangQuocTuan/web/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-define('HOST','localhost');   
-define('PORT', 3306);          
-define('USERNAME','root');
-define('PASSWORD','');
-define('DATABASE','cuahangxemay');
-?>
